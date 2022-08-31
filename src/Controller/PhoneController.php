bd75e24f5ecd25f23be304ec540c7bad11dd8719
Matthieu Mota <matthieumota@gmail.com>
2022-08-31 13:48:48 +0200

    Create entity

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,17 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\PhoneType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PhoneController extends AbstractController
 {
     #[Route('/phone', name: 'app_phone')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, ProductRepository $repository)
     {
+        // $doctrine->getRepository(Product::class)->findAll();
+        $products = $repository->findAll();
+
         return $this->render('phone/index.html.twig', [
-            'controller_name' => 'PhoneController',
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/phone/create', name: 'app_phone_new')]
+    public function create(Request $request, ManagerRegistry $doctrine, EntityManagerInterface $manager)
+    {
+        $product = new Product();
+        $form = $this->createForm(PhoneType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $manager = $doctrine->getManager();
+            $manager->persist($product);
+            $manager->flush();
+
+            $this->addFlash('success', $product->getName().' a été créé.');
+
+            return $this->redirectToRoute('app_phone');
+        }
+
+        return $this->render('phone/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/phone/{id}', name: 'app_phone_show')]
+    public function show(ProductRepository $repository, $id)
+    {
+        $product = $repository->find($id);
+
+        if (! $product) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('phone/show.html.twig', [
+            'product' => $product,
         ]);
     }
 }
